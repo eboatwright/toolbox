cc459220e948a680bfe02580cf8c6acdf32c6622
eboatwright <eboatwright7236@gmail.com>
2022-03-17 14:58:09 -0500

    3/17/22 2:58 PM
     - ransi
        - added Text (A struct for storing data for, and rendering text)

--- ransi/src/lib.rs
+++ ransi/src/lib.rs
@@ -1,5 +1,7 @@
 mod constants;
 mod glyph;
+mod text;
 
 pub use constants::*;
 pub use glyph::*;
+pub use text::*;
---
+++ ransi/src/text.rs
@@ -0,0 +1,36 @@
+// See glyph.rs for more details :)
+
+use crate::Glyph;
+use macroquad::prelude::*;
+
+#[derive(Clone, Default)]
+pub struct Text {
+	pub string: String,
+	pub fg_color: Color,
+	pub bg_color: Color,
+}
+
+impl Text {
+	pub fn new(string: String, fg_color: Color, bg_color: Color) -> Self {
+		Self {
+			string,
+			fg_color,
+			bg_color,
+		}
+	}
+
+	pub fn render(&self, font_texture: Texture2D, position: Vec2) {
+		let mut offset = Vec2::ZERO;
+		for c in self.string.chars() {
+			if c == '\n' {
+				// If the char is a new line, then reset the x offset and increase the y offset
+				offset.x = 0.0;
+				offset.y += 1.0;
+				continue;
+			}
+			// Create a new glyph and render it
+			Glyph::new(c, self.fg_color, self.bg_color).render(font_texture, position + offset);
+			offset.x += 1.0;
+		}
+	}
+}
